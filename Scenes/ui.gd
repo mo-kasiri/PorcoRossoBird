6f8c6a5cecc437e3122ae58027ffd962fac596bc
Mo Kasiri <mkgh7030@gmail.com>
2026-05-15 19:58:34 +0330

    Add scores and complete game over menu

--- Scenes/ui.gd
+++ Scenes/ui.gd
@@ -1,20 +1,30 @@
 extends CanvasLayer
 
 class_name UI
-
-@onready var label: Label = $MarginContainer/Label
+@onready var score_label: Label = $MarginContainer/ScoreLabel
 @onready var start_menu: Control = $StartMenu
-@onready var game_over: Control = $GameOver
+@onready var game_over_container: VBoxContainer = %GameOver
+@onready var score_final: Label = %ScoreFinal
+@onready var score_best: Label = %ScoreBest
 
-func ready() -> void:
-	label.text = "0";
-	game_over.hide();
+
+func _ready() -> void:
+	score_label.text = "0";
+	game_over_container.hide()
+	print("hide game over")
+
 	
 func update_score(point: int):
-	label.text = str(point);
+	score_final.text = str(point);
+	score_label.text = str(point);
 	
 #region public methods
 func show_gameover() -> void:
-	game_over.show();
+	game_over_container.show()
+	score_label.hide();
 	pass
 #endregion
+
+
+func _on_ok_button_pressed() -> void:
+	get_tree().reload_current_scene()
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,6 +5,10 @@ extends Node2D
 @onready var ui: UI = $UI
 
 static var score: int;
+static var best_score: int;
+
+func restart(_best_score: int):
+	pass
 
 
 func _on_player_on_game_started() -> void:
@@ -13,7 +17,7 @@ func _on_player_on_game_started() -> void:
 
 func _on_spawner_on_obstacle_crash() -> void:
 	player.stop_movement()
-	ui.show_gameover()
+	#ui.show_gameover()
 
 func _on_ground_on_player_crash() -> void:
 	spawner._stop_obstacles()
@@ -21,4 +25,7 @@ func _on_ground_on_player_crash() -> void:
 
 func _on_spawner_on_add_player_score() -> void:
 	score += 1;
+	if score > best_score:
+		best_score = score;
+		ui.score_best.text = str(best_score);
 	ui.update_score(score)
